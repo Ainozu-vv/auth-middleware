497e04e33b68159f0870ed0897cb6d925637c2d7
Ainozu-vv <simon.martin@vasvill.hu>
2026-01-16 08:36:43 +0100

    first commit

---
+++ database.js
@@ -0,0 +1,16 @@
+const {Sequelize} = require("sequelize")
+
+const sequelize=new Sequelize({
+    dialect:'sqlite',
+    storage:'./database.sqlite'
+})
+
+(async ()=>{
+    try {
+        await sequelize.authenticate()
+        console.log("DB success")
+    } catch (error) {
+        console.error(error)
+    }
+})()
+module.exports=sequelize
